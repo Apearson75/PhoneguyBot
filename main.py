edf3de2eae88807a0f9e099e93ef80f6642d492d
Phoneguytech75 <88546046+Phoneguytech75@users.noreply.github.com>
2021-08-19 11:10:26 +0100

    Added main.py

---
+++ main.py
@@ -0,0 +1,50 @@
+import discord
+import os
+import time
+import discord.ext
+from discord.utils import get
+from discord.ext import commands, tasks
+from discord.ext.commands import has_permissions,  CheckFailure, check
+#^ basic imports for other features of discord.py and python ^
+
+client = discord.Client()
+
+client = commands.Bot(command_prefix = '-') #put your own prefix here
+
+@client.event
+async def on_ready():
+    print("bot online") #will print "bot online" in the console when the bot is online
+    
+    
+@client.command()
+async def count(ctx, number : int):
+    await ctx.send(number +1)
+
+@client.command()
+async def clear(ctx, number : int):
+    await ctx.channel.purge(limit=number)   
+
+@client.command()
+async def kick(ctx, member : discord.Member):
+    try:
+        await member.kick(reason=None)
+        await ctx.send("kicked "+member.mention) #simple kick command to demonstrate how to get and use member mentions
+    except:
+        await ctx.send("bot does not have the kick members permission!")
+
+@client.command()
+async def say(ctx, *, text):
+  await ctx.send(text)
+
+@client.command()
+async def stab(ctx, person : discord.Member):
+  await ctx.send("Yo" + " " + person.mention + " " + "has been stabbed")
+  
+
+
+
+
+
+client.run(os.getenv("TOKEN")) #get your bot token and create a key named `TOKEN` to the secrets panel then paste your bot token as the value. 
+#to keep your bot from shutting down use https://uptimerobot.com then create a https:// monitor and put the link to the website that appewars when you run this repl in the monitor and it will keep your bot alive by pinging the flask server
+#enjoy!

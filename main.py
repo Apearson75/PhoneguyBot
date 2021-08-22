c922572c62ca11a63d5b19eb2df378281980f79f
Phoneguytech75 <88546046+Phoneguytech75@users.noreply.github.com>
2021-08-22 11:06:01 +0000

    update command

--- main.py
+++ main.py
@@ -19,7 +19,11 @@ slash = SlashCommand(client, sync_commands=True)
 async def on_ready():
     print("bot online") #will print "bot online" in the console when the bot is online
     
-    
+@slash.slash(name="update1", description="this command is only for updating the bot")    
+async def slashupdate(ctx):
+  await ctx.send("Why did you use it")
+
+
 @client.command()
 async def count(ctx, number : int):
     await ctx.send(number +1)

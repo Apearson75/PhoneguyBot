690288fcef54700a0ea2edb7e563201a672a4c6b
Phoneguytech75 <88546046+Phoneguytech75@users.noreply.github.com>
2021-08-23 11:37:40 +0100

    aniquote should work

--- main.py
+++ main.py
@@ -24,7 +24,7 @@ slash = SlashCommand(client, sync_commands=True)
 def ani_quote():
     response = requests.get("https://some-random-api.ml/animu/quote")
     json_data = json.loads(response.text)
-    quote = json_data["sentence"]
+    quote = json_data["sentence"] + " -" + ["characther"] + " Anime: " + ["anime"]
     return(quote)
 
 

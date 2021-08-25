2dfbc1fe4c95c329c637b68eaef7c3a9a8a8803c
Phoneguytech75 <88546046+Phoneguytech75@users.noreply.github.com>
2021-08-25 12:51:48 +0100

    Update main.py

--- main.py
+++ main.py
@@ -231,6 +231,7 @@ async def topscorer(ctx):
      headers = { 'X-Auth-Token': football_api }
      connection.request('GET', '/v2/competitions/PL/scorers?limit=1', None, headers )
      response = json.loads(connection.getresponse().read().decode())
+     name = response['scorers'][0]['player']['firstName']
      team = response['scorers'][0]['team']['name']
      await ctx.send(f'{name}  from  {team}')
 
